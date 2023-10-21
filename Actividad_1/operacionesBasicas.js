ccc5c934bfb38cb19fafdb4d57d93b304cc8c9cf
GianMarcos <gsvalencia4@gmail.com>
2023-10-21 18:47:41 -0500

    se realizo el ejercicio de operaciones basicas

--- Actividad_1/operacionesBasicas.js
+++ Actividad_1/operacionesBasicas.js
@@ -1,7 +1,16 @@
 'use strict'
 
-let numero1 = 5;
+let numero1 = 525;
 let numero2 = 5;
 
 let resultado = numero1 + numero2;
-console.log
+console.log('Suma:' +resultado);
+
+resultado = numero1 - numero2;
+console.log('Resta:' +resultado);
+
+resultado = numero1 * numero2;
+console.log('Multiplicación:' +resultado);
+
+resultado = numero1 / numero2;
+console.log('División:' +resultado);
